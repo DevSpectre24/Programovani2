4909d179f5b06d9658c9b55d2683b04471e29c43
novak <novak7@uhk.cz>
2025-06-12 17:17:05 +0200

    Stránky error, Custom ErrorController

---
+++ src/main/java/cz/uhk/kpro2/controller/CustomErrorController.java
@@ -0,0 +1,29 @@
+package cz.uhk.kpro2.controller;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class CustomErrorController implements ErrorController {
+
+    @RequestMapping("/error")
+    public String handleError(HttpServletRequest request, Model model) {
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+
+        if (status != null) {
+            Integer statusCode = Integer.valueOf(status.toString());
+
+            if (statusCode == 404) {
+                return "404";
+            }
+            if (statusCode == 403) {
+                return "403";
+            }
+        }
+        return "error";
+    }
+}
